579a7ffd47ca13aad1ae3fda6ea4f8159dafb716
Arthur <davila.arthur05@gmail.com>
2023-11-30 14:16:24 +0800

    Added sensors and displays contructors and attributes

--- src/sensor.py
+++ src/sensor.py
@@ -1,2 +1,16 @@
 class Sensor:
+    def __init__(self, id, car_park, is_active=False):
+        self.id = id
+        self.car_park = car_park
+        self.is_active = is_active
+
+    def __str__(self):
+        return f"{self.id}: Sensor {'is active' if self.is_active else 'is not active'}"
+
+
+class EntrySensor(Sensor):
+    ...
+
+
+class ExitSensor(Sensor):
     ...
